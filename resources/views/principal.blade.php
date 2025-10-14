4e7941893fe87dee069dae15a0fbb7b43b2382e1
Matias-hue <ramirezmatias1122@gmail.com>
2025-10-14 19:38:40 -0300

    Botones comentados (solo dashboard) y CSS agregar gasto

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -8,8 +8,8 @@
 
     <div class="menu" style="margin-top: 20px;">
         <!-- Botones principales -->
-        <button onclick="location.href='{{ route('presupuestos.crear') }}'" style="margin: 5px; padding: 10px 20px;">Crear/Editar Presupuesto</button>
-        <button onclick="location.href='{{ route('gastos.crear') }}'" style="margin: 5px; padding: 10px 20px;">Agregar Gasto</button>
+        <!-- <button onclick="location.href='{{ route('presupuestos.crear') }}'" style="margin: 5px; padding: 10px 20px;">Crear/Editar Presupuesto</button>
+        <button onclick="location.href='{{ route('gastos.crear') }}'" style="margin: 5px; padding: 10px 20px;">Agregar Gasto</button> -->
         <button onclick="location.href='{{ route('dashboard') }}'" style="margin: 5px; padding: 10px 20px;">Ver Dashboard</button>  
         <button onclick="location.href='{{ route('gastos.historialSemanas') }}'" style="margin: 5px; padding: 10px 20px;">Gastos Semanales</button>
     </div>
